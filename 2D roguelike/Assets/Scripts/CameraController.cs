34afb70b03cd0976821d78a0247f81c4801c5c22
tuchtenh <50889613+tuchtenh@users.noreply.github.com>
2023-03-30 19:22:26 +0300

    Camera follows player
    CameraController script to follow the player when moving

---
+++ 2D roguelike/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float FollowSpeed = 5f;
+
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = new Vector3(target.position.x,target.position.y, -10f);
+        transform.position = Vector3.Slerp(transform.position,newPos,FollowSpeed*Time.deltaTime);
+
+    }
+}
+
+
