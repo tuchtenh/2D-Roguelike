026494e11687169a1e6d820c70e7c022e23af2e0
tuchtenh <50889613+tuchtenh@users.noreply.github.com>
2023-04-18 02:11:59 +0300

    EnemySpawner

--- 2D roguelike/Assets/Characters/EnemyAI/Skeleton/Skeleton.cs
+++ 2D roguelike/Assets/Characters/EnemyAI/Skeleton/Skeleton.cs
@@ -38,5 +38,4 @@ public class Skeleton : MonoBehaviour
         }
     }
 
-    
 }
---
+++ 2D roguelike/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject skeletonPrefab;
+
+    [SerializeField]
+    private float skeletonInterval = 15f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(spawnEnemy(skeletonInterval, skeletonPrefab));
+    }
+
+    private IEnumerator spawnEnemy(float interval, GameObject enemy)
+    {
+        yield return new WaitForSeconds(interval);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-4f, 4), Random.Range(-4f, 10f), 0), Quaternion.identity);
+        StartCoroutine(spawnEnemy(interval, enemy));
+    }
+}
