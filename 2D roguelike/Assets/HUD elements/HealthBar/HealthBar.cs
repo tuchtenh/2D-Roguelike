9242ead8e8b886aa76e51813a476b02b14eab11e
tuchtenh <50889613+tuchtenh@users.noreply.github.com>
2023-04-24 17:17:00 +0300

    Health number display

--- 2D roguelike/Assets/HUD elements/HealthBar/HealthBar.cs
+++ 2D roguelike/Assets/HUD elements/HealthBar/HealthBar.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,15 +8,23 @@ public class HealthBar : MonoBehaviour
 {
 
     public Slider slider;
+    public TextMeshProUGUI hp;
 
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
         slider.value = health;
+        UpdateHealthDisplay();
     }
 
     public void SetHealth(int health)
     {
         slider.value = health;
+        UpdateHealthDisplay();
+    }
+
+    public void UpdateHealthDisplay()
+    {
+        hp.text = slider.value + "/" + slider.maxValue;
     }
 }
